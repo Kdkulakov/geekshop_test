c09ae31d7edbb2c97da12ce165e18d73b290c1e3
Kulakov <kdkulakov@gmail.com>
2021-05-25 22:44:42 +0300

    add read

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.staticfiles',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
